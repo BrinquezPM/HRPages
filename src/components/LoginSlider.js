81baff18cd00884ffd92e81f681803acfa530866
NgalotJ <ngalot.johnvictor@gmail.com>
2023-04-28 09:36:01 +0800

    Transfer Login UI to here
    initial transfer

---
+++ src/components/LoginSlider.js
@@ -0,0 +1,53 @@
+import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from 'react';
+import Slide1 from './Images/Login/Interview-rafiki 1.svg'
+import Slide2 from './Images/Login/People search-rafiki 1.svg'
+import Slide3 from './Images/Login/Podcast-rafiki 1.svg'
+
+const data = [
+    {
+        image: Slide1,
+        caption: 'Find top candidates quickly',
+        //description: ''
+    },
+    {
+        image: Slide2,
+        caption: 'Manage users efficiently',
+        //description: ''
+    },
+    {
+        image: Slide3,
+        caption: 'Streamline application process',
+        //description: ''
+    }
+]
+
+function LoginSlider() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect} variant='dark'>
+            {data.map((slide, i) => {
+                return (
+                    <Carousel.Item>
+                        <img
+                            className = 'd-block w-100'
+                            src = {slide.image}
+                            alt = 'slides'
+                        ></img>
+                        <Carousel.Caption>
+                            <h5 >{slide.caption}</h5>
+                            {/* <p>{slide.description}</p> */}
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                )
+            })}
+        </Carousel>
+    )
+}
+
+export default LoginSlider;
