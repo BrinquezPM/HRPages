5c1c8557e10c0b6bf8df8beb0af6950babb746dd
NgalotJ <ngalot.johnvictor@gmail.com>
2023-04-29 14:17:58 +0800

    UI mostly done
    UI mostly done, the spacing on values of username and contact number is iffy

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { Button, Col, Container, Row, Stack } from 'react-bootstrap';
 import "../App.css";
+import MailIcon from '../Images/mailLogo.svg';
+import EditIcon from '../Images/pencil-outline.svg';
+import DeactivateIcon from '../Images/trash-can-outline.svg';
+import UsernameIcon from '../Images/account-outline.svg';
+import ContactIcon from '../Images/phone-outline.svg';
+import SampleProfile from '../Images/profile-pic-sample.svg';
 
 function Profile() {
 
@@ -11,51 +17,31 @@ function Profile() {
     // }
     
     return(
-        <div style={{marginLeft: 270}}>
-        <Container>
-            <Row>
-                <Col>
-                    {/* <h1>{currProfile ? 'Profile' : 'User Profile'}</h1> */}
-                    <h1>Profile1</h1>
-                </Col>
-                <Col>
-                    <h1>Profile</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h6>profile pic</h6>
-                </Col>
-                <Col>
-                    <h4>user.name</h4>
-                    <h6>icon.mail</h6>
-                    <Button variant='success'>
-                        Edit Profile
-                    </Button>
-                    <Button variant='danger'>
-                        Deactivate
-                    </Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h6>icon.username</h6>
-                </Col>
-                <Col>
-                    <h5>Username</h5>
-                    <h6>user.username</h6>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <h6>icon.contact number</h6>
-                </Col>
-                <Col>
-                    <h5>Contact Number</h5>
-                    <h6>user.contactNumber</h6>
-                </Col>
-            </Row>
-        </Container>
+        <div style={{marginLeft: 270}} className='App container'>
+        <Stack gap={4}>
+            <Stack gap={1}>
+                <h1>Profile</h1>
+                <Stack direction='horizontal' gap={3}>
+                    <img src={SampleProfile} alt='sample profile' />
+                    <div>
+                        <h4>user.name</h4>
+                        <h6><img src={MailIcon} alt='Mail' /> user.email</h6>
+                        <Button variant='success'>
+                            <img src={EditIcon} alt='Edit Profile' /> Edit Profile
+                        </Button> {'\t'}
+                        <Button variant='danger'>
+                            <img src={DeactivateIcon} alt='Deactivate' /> Deactivate
+                        </Button>
+                    </div>
+                </Stack>
+            </Stack>
+            <Stack>
+                <h5><img src={UsernameIcon} alt='username' /> Username</h5>
+                <p><span>user.username</span></p>
+                <h5><img src={ContactIcon} alt='contact number' /> Contact Number</h5>
+                <p><span>user.contactNumber</span></p> 
+            </Stack>
+        </Stack>
         </div>
     )
 }
