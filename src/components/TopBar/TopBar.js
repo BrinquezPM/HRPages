6757fd02bb0270e18e313512639b5ecc836a2111
NgalotJ <ngalot.johnvictor@gmail.com>
2023-05-01 23:49:09 +0800

    Update TopBar.js
    fix, copied brinquez

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -2,31 +2,40 @@ import "./TopBar.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { useEffect, useState } from "react";
 import Modal from "../Modal/Modal";
+import { useLocation } from "react-router";
 
 const TopBar = (props) => {
   const [isLogoutModalActive, setIsLogoutModalActive] = useState(false);
   const toggleLogoutModal = () => {
     setIsLogoutModalActive(!isLogoutModalActive);
   };
-  const [isProfileActive,setIsProfileActive] = useState(false);
-  useEffect(() => {
-    // Function to update the state based on the current pathname
-    function updateIsProfileActive(){
-      setIsProfileActive(window.location.pathname === "/profile");
-    }
-    // Event listener to the window to listen for changes to the pathname
-    window.addEventListener("popstate", updateIsProfileActive);
-    // Calling the update function once to set the initial state based on the current pathname
-    updateIsProfileActive();
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("popstate", updateIsProfileActive);
-    };
-  }, []);
+  // const [isProfileActive,setIsProfileActive] = useState(false);
+  // useEffect(() => {
+  //   // Function to update the state based on the current pathname
+  //   function updateIsProfileActive(){
+  //     setIsProfileActive(window.location.pathname === "/profile" || window.location.pathname === "/user");
+  //   }
+  //   // Event listener to the window to listen for changes to the pathname
+  //   window.addEventListener("popstate", updateIsProfileActive);
+  //   // Calling the update function once to set the initial state based on the current pathname
+  //   updateIsProfileActive();
+  //   // Clean up the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener("popstate", updateIsProfileActive);
+  //   };
+  // }, []);
+  const curPath = useLocation();
+  let searchVisibility = false;
+  if(curPath.pathname === "/user" || curPath.pathname === "/profile"){
+    searchVisibility = "hidden";
+  }
+  else{
+    searchVisibility = "visible"
+  }
   return (
     <div className="top-bar">
       <SearchBar 
-        searchVisibility={`${isProfileActive ? "hidden" : "visible"}`}
+        searchVisibility={searchVisibility}
       />
       <button onClick={toggleLogoutModal} id="logout-btn">
         <img
