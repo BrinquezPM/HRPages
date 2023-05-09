28ea2c627afa181989ba4949acb6ae1a71f8993f
BrinquezPM <75445572+BrinquezPM@users.noreply.github.com>
2023-05-09 23:17:16 +0800

    minor changes to main layout

--- src/routes/MainLayout/MainLayout.js
+++ src/routes/MainLayout/MainLayout.js
@@ -4,14 +4,12 @@ import TopBar from "../../components/TopBar/TopBar";
 import UserForm from "../../components/UserForm/UserForm";
 import ApplicantDetails from "../../components/ApplicantDetails/ApplicantDetails";
 import ProfilePage from "../ProfilePage/ProfilePage";
-import Applicants from "../Applicants";
 const MainLayout = () => {
   return (
     <div className="row-container">
       <Sidebar />
       <div className="main-content">
         <TopBar />
-        <Applicants/>
         {/* {<UserForm formFunction="Create" />} */}
         {/* <ApplicantDetails /> */}
         {/* <ProfilePage /> */}
